53c6f6a6678166d31ea3a27ce3521383eebcd398
Ibrahim Siddique <ibrahimsiddique0202@gmail.com>
2026-08-13 01:10:41 +0530

    ejs setup

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const port = 8000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/search", (req, res) => {
+    res.render("search.ejs");
+});
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+});
+
+app.get("/hello", (req, res) => {
+    res.send("this is hello page");
+});
 
 app.listen(port, () => {
     console.log(`listing on port ${port}`);
