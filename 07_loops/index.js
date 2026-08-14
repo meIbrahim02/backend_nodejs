d479662bbb3cc928da3cb24547225a7647fef30a
Ibrahim Siddique <ibrahimsiddique0202@gmail.com>
2026-08-15 00:51:47 +0530

    loops in ejs

---
+++ 07_loops/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 8000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"))
+
+app.get("/ig/:username", (req, res) => {
+    const followers = ["adam", "bob", "tom", "jerry"];
+    let {username} = req.params;
+    res.render("home.ejs", {username, followers});
+})
+
+app.listen(port, ()=>{
+    console.log(`app is listining to port ${port}`)
+})
