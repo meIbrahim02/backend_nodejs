ea150334c13f81d346ee6ece72f356d7ede402ec
Ibrahim Siddique <ibrahimsiddique0202@gmail.com>
2026-08-17 23:39:28 +0530

    app setup done

--- 05_REST/index.js
+++ 05_REST/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const path = require("path");
+
+app.use(express.urlencoded({extended : true}));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set(express.static(path.join(__dirname, "views")));
+
+app.get('/', (req, res)=>{
+    res.send("server is working well!")
+})
 
 app.listen(port, ()=>{
     console.log(`app is listning on port ${port}`);
