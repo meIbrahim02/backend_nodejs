e27d3c5de7246f2192aeb12b65be79d1606900b5
Ibrahim Siddique <ibrahimsiddique0202@gmail.com>
2026-08-17 23:54:57 +0530

    creating a rest api

--- 05_REST/index.js
+++ 05_REST/index.js
@@ -2,15 +2,33 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const path = require("path");
+let posts = [
+    {
+        username : "ibrahim siddique",
+        content : "nothing"
+    },
+    {
+        username : "bob",
+        content : "something"
+    },
+    {
+        username : "william",
+        content : "everything"
+    },
+]
 
 app.use(express.urlencoded({extended : true}));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.set(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req, res)=>{
     res.send("server is working well!")
+});
+
+app.get('/posts', (req, res)=>{
+    res.render("index.ejs", {posts: posts});
 })
 
 app.listen(port, ()=>{
